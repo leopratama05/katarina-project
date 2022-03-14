cda2478c269b315ce6563884a3e9c8dec381a26e
Herry Prasetyo <herry081288@gmail.com>
2022-03-14 11:02:13 +0700

    login done

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,8 @@
     <div class="card-body login-card-body">
         <p class="login-box-msg">Sign in to start your session</p>
 
-        <form action="../../index3.html" method="post">
+        <form action="#" method="post">
+            @csrf
             <div class="input-group mb-3">
                 <input type="email" class="form-control" placeholder="Email">
                 <div class="input-group-append">
