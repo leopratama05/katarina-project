456e9ce10fa507b63e79b43128af44abb4a0b5e4
Herry Prasetyo <herry081288@gmail.com>
2022-03-16 01:07:11 +0700

    homePage

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -9,12 +9,12 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-sm-6">
-                    <h1>Product List</h1>
+                    <h1>Home Page</h1>
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item"><a href="#">Product</a></li>
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
                     </ol>
                 </div>
             </div>
@@ -26,53 +26,68 @@
     <section class="content">
         <div class="container-fluid">
             <div class="row">
-                <div class="col-12">
-                    <div class="card">
-                        <div class="card-header">
-                            <a href="{{ route('product.create') }}" class="btn btn-primary card-title">Create Product</a>
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-info">
+                        <div class="inner">
+                            <h3>#</h3>
+                            <p>Orders Count</p>
                         </div>
-                        <div class="card-body">
-                            <table class="table table-bordered">
-                                <thead>
-                                    <tr>
-                                        <th>#</th>
-                                        <th>Name</th>
-                                        <th>Image</th>
-                                        <th>Barcode</th>
-                                        <th>Price</th>
-                                        <th>Quantity</th>
-                                        <th>Status</th>
-                                        <th>Action</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @forelse ($product as $pr)
-                                        <tr>
-                                            <td>{{ $loop->iteration }}</td>
-                                            <td>{{ $pr->name }}</td>
-                                            <td><img src="{{ Storage::url($pr->image) }}" width="100"></td>
-                                            <td>{{ $pr->description }}</td>
-
-                                            <td><img src="{{ Storage::url($pr->image) }}" widht="100"></td>
-
-                                            <td>{{ $pr->barcode }}</td>
-                                            <td>{{ $pr->price }}</td>
-                                            <td>{{ $pr->quantity }}</td>
-                                            <td><a href="{{ route('product.edit', $pr->id) }}"
-                                                    class="btn btn-warning">Edit</a>|
-                                                <a href="{{route('product.destroy', $pr->id)}}" class="btn btn-danger">Delete</a>
-                                                </td>
-                                        </tr>
-                                    @empty
-                                        <tr>
-                                            <td colspan="8" class="text-center">Belum Ada Data</td>
-                                        </tr>
-                                    @endforelse
-                                </tbody>
-                            </table>
+                        <div class="icon">
+                            <i class="ion ion-bag"></i>
                         </div>
+                        <a href="#" class="small-box-footer">More info <i
+                                class="fas fa-arrow-circle-right"></i></a>
                     </div>
                 </div>
+                <!-- ./col -->
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-success">
+                        <div class="inner">
+                            <h3>#</h3>
+                            <p>Income</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-stats-bars"></i>
+                        </div>
+                        <a href="#" class="small-box-footer">More info <i
+                                class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+                <!-- ./col -->
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-danger">
+                        <div class="inner">
+                            <h3>#</h3>
+
+                            <p>Income Today</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-pie-graph"></i>
+                        </div>
+                        <a href="{{ route('orders.index') }}" class="small-box-footer">More info <i
+                                class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+                <!-- ./col -->
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-warning">
+                        <div class="inner">
+                            <h3>0</h3>
+
+                            <p>Customers Count</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-person-add"></i>
+                        </div>
+                        <a href="#" class="small-box-footer">More info <i
+                                class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+                <!-- ./col -->
             </div>
         </div>
     </section>
