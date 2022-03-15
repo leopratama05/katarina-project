07a88821fc1baad055eeac1a6c88baf420368de2
Herry Prasetyo <herry081288@gmail.com>
2022-03-16 01:16:32 +0700

    secureassetadd

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -15,7 +15,7 @@
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
     <!-- Tempusdominus Bootstrap 4 -->
     <link rel="stylesheet"
-        href="{{ asset('admlte/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') }}">
+        href="{{ secure_asset('admlte/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') }}">
     <!-- iCheck -->
     <link rel="stylesheet" href="{{ secure_asset('admlte/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
     <!-- JQVMap -->
@@ -35,7 +35,7 @@
 
         <!-- Preloader -->
         <div class="preloader flex-column justify-content-center align-items-center">
-            <img class="animation__shake" src="{{ asset('admlte') }}/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+            <img class="animation__shake" src="{{ secure_asset('admlte') }}/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
         </div>
 
         <!-- Navbar -->
@@ -73,31 +73,31 @@
         $.widget.bridge('uibutton', $.ui.button)
     </script>
     <!-- Bootstrap 4 -->
-    <script src="{{ asset('admlte') }}/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ secure_asset('admlte') }}/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
     <!-- ChartJS -->
-    <script src="{{ asset('admlte') }}/plugins/chart.js/Chart.min.js"></script>
+    <script src="{{ secure_asset('admlte') }}/plugins/chart.js/Chart.min.js"></script>
     <!-- Sparkline -->
-    <script src="{{ asset('admlte') }}/plugins/sparklines/sparkline.js"></script>
+    <script src="{{ secure_asset('admlte') }}/plugins/sparklines/sparkline.js"></script>
     <!-- JQVMap -->
-    <script src="{{ asset('admlte') }}/plugins/jqvmap/jquery.vmap.min.js"></script>
-    <script src="{{ asset('admlte') }}/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+    <script src="{{ secure_asset('admlte') }}/plugins/jqvmap/jquery.vmap.min.js"></script>
+    <script src="{{ secure_asset('admlte') }}/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
     <!-- jQuery Knob Chart -->
-    <script src="{{ asset('admlte') }}/plugins/jquery-knob/jquery.knob.min.js"></script>
+    <script src="{{ secure_asset('admlte') }}/plugins/jquery-knob/jquery.knob.min.js"></script>
     <!-- daterangepicker -->
-    <script src="{{ asset('admlte') }}/plugins/moment/moment.min.js"></script>
-    <script src="{{ asset('admlte') }}/plugins/daterangepicker/daterangepicker.js"></script>
+    <script src="{{ secure_asset('admlte') }}/plugins/moment/moment.min.js"></script>
+    <script src="{{ secure_asset('admlte') }}/plugins/daterangepicker/daterangepicker.js"></script>
     <!-- Tempusdominus Bootstrap 4 -->
-    <script src="{{ asset('admlte') }}/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+    <script src="{{ secure_asset('admlte') }}/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
     <!-- Summernote -->
-    <script src="{{ asset('admlte') }}/plugins/summernote/summernote-bs4.min.js"></script>
+    <script src="{{ secure_asset('admlte') }}/plugins/summernote/summernote-bs4.min.js"></script>
     <!-- overlayScrollbars -->
-    <script src="{{ asset('admlte') }}/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+    <script src="{{ secure_asset('admlte') }}/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
     <!-- AdminLTE App -->
-    <script src="{{ asset('admlte') }}/js/adminlte.js"></script>
+    <script src="{{ secure_asset('admlte') }}/js/adminlte.js"></script>
     <!-- AdminLTE for demo purposes -->
-    <script src="{{ asset('admlte') }}/js/demo.js"></script>
+    <script src="{{ secure_asset('admlte') }}/js/demo.js"></script>
     <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-    <script src="{{ asset('admlte') }}/js/pages/dashboard.js"></script>
+    <script src="{{ secure_asset('admlte') }}/js/pages/dashboard.js"></script>
 </body>
 
 </html>
