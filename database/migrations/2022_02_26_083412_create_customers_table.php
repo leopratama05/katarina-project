b2d52801861e90f7fe893057cfc97e8663e2d09a
Herry Prasetyo <herry081288@gmail.com>
2022-03-15 11:25:51 +0700

    testserius

--- database/migrations/2022_02_26_083412_create_customers_table.php
+++ database/migrations/2022_02_26_083412_create_customers_table.php
@@ -15,12 +15,9 @@ class CreateCustomersTable extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
-
             $table->string('firstname')->nullable();
             $table->string('lastname')->nullable();
-            $table->string('email')->nullable();
-            $table->string('phone')->nullable();
-            $table->string('address')->nullable();
+
             $table->foreignId('user_id')->constrained('users');
             $table->timestamps();
             // $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
