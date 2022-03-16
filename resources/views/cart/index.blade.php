cd7b8ce0a42b71e549c58e7a31138f43e65adc0d
Herry Prasetyo <herry081288@gmail.com>
2022-03-16 10:20:51 +0700

    cartindex

---
+++ resources/views/cart/index.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.master')
+
+@section('title','POS Ujikom')
+
+@section('content-header')
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-6">
+                    <h1>Home Page</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
+
+@section('content')
+    <div id="cart"></div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,7 @@ Route::group(['middleware' => ['auth', 'level']], function () {
     Route::get('product/destroy/{id}', [\App\Http\Controllers\ProductController::class, 'destroy'])->name('product.destroy');
 
     //cart
-
+    Route::get('/cart',[App\Http\Controllers\Car])
 });
 
 Route::group(['middleware'=>['auth','manager']], function(){
